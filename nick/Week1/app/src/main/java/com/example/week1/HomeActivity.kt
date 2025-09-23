e1385d90814bd4f4350e102e66009050ca2fafcb
Nick9417 <kimjaehwan0920@gmail.com>
2025-09-24 00:51:04 +0900

    [닉/#1] chore 1주차 미션 피드백 수정

--- nick/Week1/app/src/main/java/com/example/week1/HomeActivity.kt
+++ nick/Week1/app/src/main/java/com/example/week1/HomeActivity.kt
@@ -10,51 +10,51 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.week1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val text = "hello toast"
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(this, text, duration)
+
+        val intent = Intent(this, JoyActivity::class.java)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val text = "hello toast"
-        val duration = Toast.LENGTH_SHORT
-        val toast = Toast.makeText(this, text, duration)
-
-        val intent = Intent(this, MainActivity2::class.java)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.joyImage.setOnClickListener {
-            binding.joyText.setTextColor(Color.YELLOW)
+        binding.homeJoyIv.setOnClickListener {
+            binding.homeJoyTv.setTextColor(Color.YELLOW)
             toast.show()
             startActivity(intent)
         }
 
-        binding.exitedImage.setOnClickListener {
-            binding.exitedText.setTextColor(Color.BLUE)
+        binding.homeExcitedIv.setOnClickListener {
+            binding.homeExcitedTv.setTextColor(Color.BLUE)
             toast.show()
         }
 
-        binding.sosoImage.setOnClickListener {
-            binding.sosoText.setTextColor(Color.CYAN)
+        binding.homeSosoIv.setOnClickListener {
+            binding.homeSosoTv.setTextColor(Color.CYAN)
             toast.show()
         }
 
-        binding.nervousImage.setOnClickListener {
-            binding.nervousText.setTextColor(Color.GREEN)
+        binding.homeNervousIv.setOnClickListener {
+            binding.homeNervousTv.setTextColor(Color.GREEN)
             toast.show()
         }
 
-        binding.madImage.setOnClickListener {
-            binding.madText.setTextColor(Color.RED)
+        binding.homeMadIv.setOnClickListener {
+            binding.homeNervousTv.setTextColor(Color.RED)
             toast.show()
         }
     }
