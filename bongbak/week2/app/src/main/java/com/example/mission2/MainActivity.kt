5c206a55b39161a069ed6a1c3370c7f49b564028
bongbak <eunchae0923@naver.com>
2025-09-30 01:07:02 +0900

    2주차 미션 수행

---
+++ bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.mission2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mission2.databinding.ActivityMainBinding
+import com.example.mission2.HomeFragment
+import com.example.mission2.LookFragment
+import com.example.mission2.LockerFragment
+import com.example.mission2.SearchFragment
+import com.example.mission2.Song
+import com.example.mission2.SongActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBottomNavigation()
+
+        val song = Song(
+            binding.mainMiniplayerTitleTv.text.toString(),
+            binding.mainMiniplayerSingerTv.text.toString()
+        )
+
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+            intent.putExtra("title", song.title)
+            intent.putExtra("singer", song.singer)
+            startActivity(intent)
+        }
+    }
+
+    private fun initBottomNavigation(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, HomeFragment())
+            .commitAllowingStateLoss()
+
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, HomeFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.lookFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LookFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, SearchFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.lockerFragment -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, LockerFragment())
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true // 2. 오타 수정
+                }
+            }
+            false
+        }
+    }
+}
---
+++ bongbak/week2/app/src/main/java/com/example/mission2/Song.kt
@@ -0,0 +1,7 @@
+package com.example.mission2
+
+data class Song(
+    val title : String ="",
+    val singer : String="",
+
+)
