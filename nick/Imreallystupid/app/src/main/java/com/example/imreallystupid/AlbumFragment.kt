cbe1533912734ed2c5d4c7f57f0e1c16d6b14432
Nick9417 <kimjaehwan0920@gmail.com>
2025-09-30 08:39:37 +0900

    mission/#3 AlbumFragment, LockerFragment UI 구현

---
+++ nick/Imreallystupid/app/src/main/java/com/example/imreallystupid/AlbumFragment.kt
@@ -0,0 +1,25 @@
+package com.example.imreallystupid
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.imreallystupid.databinding.FragmentAlbumBinding
+
+
+
+class AlbumFragment : Fragment() {
+
+    lateinit var binding: FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
