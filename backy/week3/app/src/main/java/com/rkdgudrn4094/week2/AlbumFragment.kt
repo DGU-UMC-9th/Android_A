ef7fd07ff0fd1eeac6eb1846fc072f55b181cf9c
kang <haengoo@naver.com>
2025-09-30 16:25:38 +0900

    [Backy/#31] 2주차 프로젝트 복사

---
+++ backy/week3/app/src/main/java/com/rkdgudrn4094/week2/AlbumFragment.kt
@@ -0,0 +1,34 @@
+package com.rkdgudrn4094.week2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.rkdgudrn4094.week2.databinding.FragmentAlbumBinding
+
+class AlbumFragment : Fragment() {
+    lateinit var binding: FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        binding.albumBackIv.setOnClickListener {
+            (context as MainActivity).supportFragmentManager.beginTransaction().replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
+        }
+
+        /*
+        binding.songLalacLayout.setOnClickListener {
+            Toast.makeText(activity, "LILAC", Toast.LENGTH_SHORT).show()
+        }*/
+
+        binding.albumMusicTitleTv.text=arguments?.getString("title")
+        binding.albumSingerNameTv.text=arguments?.getString("singer")
+
+        return binding.root
+    }
+}
