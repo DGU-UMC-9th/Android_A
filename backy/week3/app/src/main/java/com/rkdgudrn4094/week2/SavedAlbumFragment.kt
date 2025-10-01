605d64d4e074cc856ba7c064862524079558686a
kang <haengoo@naver.com>
2025-10-01 18:32:16 +0900

    [Backy/#31] mission 보관함 화면(LockerFragment)을 TabLayout과 Viewpager로 구현하기

--- backy/week3/app/src/main/java/com/rkdgudrn4094/week2/SavedAlbumFragment.kt
+++ backy/week3/app/src/main/java/com/rkdgudrn4094/week2/SavedAlbumFragment.kt
@@ -5,17 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rkdgudrn4094.week2.databinding.FragmentSavedsongBinding
+import com.rkdgudrn4094.week2.databinding.FragmentSavedalbumBinding
 
-class SavedSongFragment: Fragment() {
-    lateinit var binding: FragmentSavedsongBinding
+
+class SavedAlbumFragment: Fragment(){
+    lateinit var binding: FragmentSavedalbumBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding =FragmentSavedsongBinding.inflate(inflater,container,false)
+        binding = FragmentSavedalbumBinding.inflate(inflater, container, false)
 
         return binding.root
     }
--- backy/week3/app/src/main/java/com/rkdgudrn4094/week2/SongFileFragment.kt
+++ backy/week3/app/src/main/java/com/rkdgudrn4094/week2/SongFileFragment.kt
@@ -5,17 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.rkdgudrn4094.week2.databinding.FragmentSavedsongBinding
+import com.rkdgudrn4094.week2.databinding.FragmentSongfileBinding
 
-class SavedSongFragment: Fragment() {
-    lateinit var binding: FragmentSavedsongBinding
+
+class SongFileFragment: Fragment(){
+    lateinit var binding: FragmentSongfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding =FragmentSavedsongBinding.inflate(inflater,container,false)
+        binding = FragmentSongfileBinding.inflate(inflater, container, false)
 
         return binding.root
     }
