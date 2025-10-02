a18b83d790958889c1b3a9b830a51cd7619a5f47
bongbak <eunchae0923@naver.com>
2025-10-02 19:27:45 +0900

    안녕하세요.txt 추가

--- bongbak/week2/app/src/main/java/com/example/mission2/HomeFragment.kt
+++ bongbak/week2/app/src/main/java/com/example/mission2/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.mission2.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
@@ -23,6 +24,11 @@ class HomeFragment : Fragment() {
                 .commitAllowingStateLoss()
         }
 
+        val bannerAdapter= BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homeBannerVp.adapter=bannerAdapter
+        binding.homeBannerVp.orientation= ViewPager2.ORIENTATION_HORIZONTAL
         return binding.root
     }
 
--- bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
+++ bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
@@ -35,39 +35,5 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, HomeFragment())
             .commitAllowingStateLoss()
-
-        fragmentManager.executePendingTransactions()
-
-        binding.mainBnv.setOnItemSelectedListener{ item ->
-            when (item.itemId) {
-
-                R.id.homeFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, HomeFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-
-                R.id.lookFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, LookFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.searchFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, SearchFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-                R.id.lockerFragment -> {
-                    supportFragmentManager.beginTransaction()
-                        .replace(R.id.main_frm, LockerFragment())
-                        .commitAllowingStateLoss()
-                    return@setOnItemSelectedListener true
-                }
-            }
-            false
         }
     }
-}
