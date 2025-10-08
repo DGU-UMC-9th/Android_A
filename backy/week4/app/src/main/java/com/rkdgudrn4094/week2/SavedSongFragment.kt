90baa163cda114a342a26bf74aa64e7ca9001f97
kang <haengoo@naver.com>
2025-10-09 00:00:47 +0900

    [Backy/#41] mission 보관함 화면(LockerFragment) 저장한 곡 탭에 들어 노래 RecyclerView 레이아웃 및 RecyclerView 만들기

--- backy/week4/app/src/main/java/com/rkdgudrn4094/week2/SavedSongFragment.kt
+++ backy/week4/app/src/main/java/com/rkdgudrn4094/week2/SavedSongFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rkdgudrn4094.week2.databinding.FragmentSavedsongBinding
 
 class SavedSongFragment: Fragment() {
     lateinit var binding: FragmentSavedsongBinding
+    private var albumDatas = ArrayList<Album>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,6 +18,19 @@ class SavedSongFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding =FragmentSavedsongBinding.inflate(inflater,container,false)
+        albumDatas.apply{
+            add(Album("Butter", "방탄소년단 (BTS)", R.drawable.img_album_exp))
+            add(Album("Lilac", "아이유 (IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파 (AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv", "방탄소년단 (BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom", "모모랜드 (MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend", "태연 (Tae Yeon)", R.drawable.img_album_exp6))
+        }
+
+        val savedSongRVAdapter = SavedSongRVAdapter(albumDatas)
+        binding.savedsongSongRv.adapter = savedSongRVAdapter
+        binding.savedsongSongRv.layoutManager = LinearLayoutManager(context,
+            LinearLayoutManager.VERTICAL, false)
 
         return binding.root
     }
---
+++ backy/week4/app/src/main/java/com/rkdgudrn4094/week2/SavedSongRVAdapter.kt
@@ -0,0 +1,44 @@
+package com.rkdgudrn4094.week2
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.rkdgudrn4094.week2.databinding.ItemAlbumBinding
+import com.rkdgudrn4094.week2.databinding.ItemSavedsongBinding
+
+class SavedSongRVAdapter(private val albumList: ArrayList<Album>): RecyclerView.Adapter<SavedSongRVAdapter.ViewHolder>() {
+
+    interface MyItemClickListener{
+        fun onItemClick(album: Album)
+    }
+
+    private lateinit var mItemClickListener: MyItemClickListener
+    fun setMyItemClickListener(itemClickListener: MyItemClickListener){
+        mItemClickListener = itemClickListener
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): SavedSongRVAdapter.ViewHolder {
+        val binding: ItemSavedsongBinding = ItemSavedsongBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: SavedSongRVAdapter.ViewHolder, position: Int) {
+        holder.bind(albumList[position])
+        holder.itemView.setOnClickListener {
+            mItemClickListener.onItemClick(albumList[position])
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return albumList.size
+    }
+
+    inner class ViewHolder(val binding: ItemSavedsongBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(album: Album){
+            binding.itemSongTitleTv.text = album.title
+            binding.itemSongSingerTv.text = album.singer
+            binding.itemSongAlbumImgIv.setImageResource(album.coverImg!!)
+        }
+    }
+}
