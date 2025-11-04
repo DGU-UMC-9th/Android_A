ebfde97a74228c945b53e97ac55b9490f6b3152c
bongbak <eunchae0923@naver.com>
2025-11-05 01:57:33 +0900

    [mission] 6주차 메인화면 SeekBar 구현 추가

--- bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
+++ bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
@@ -4,9 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mission2.databinding.ActivityMainBinding
+import com.google.gson.Gson
+
 class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
 
     lateinit var binding: ActivityMainBinding
+    private var song:Song = Song()
+    private var gson:Gson= Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -80,9 +84,26 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
             false
         }
     }
+    private fun setMiniPlayer(song: Song){
+        binding.mainMiniplayerTitleTv.text=song.title
+        binding.mainMiniplayerSingerTv.text=song.singer
+        binding.mainMiniplayerProgressSb.progress=(song.second*100000)/song.playTime
+    }
 
     override fun onStart() {
         super.onStart()
+        val sharedPreferences=getSharedPreferences("song",MODE_PRIVATE)
+        val songJson=sharedPreferences.getString("songData",null)
+
+        song=if(songJson==null) {
+            Song("라일락", "아이유(IU)", 0, 60, false, "unvailable")
+        }else{
+            gson.fromJson(songJson,Song::class.java)
+
+        }
+        setMiniPlayer(song)
+
+
     }
 
 }
