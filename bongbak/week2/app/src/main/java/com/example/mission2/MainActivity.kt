feb47cf3bad0d59952a9c20d8cc2ea1f61237fe1
bongbak <eunchae0923@naver.com>
2025-11-18 04:46:40 +0900

    [Mission] 7주차 미션 수행 - MainActivity.kt Next, Previous 동기화

--- bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
+++ bongbak/week2/app/src/main/java/com/example/mission2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.mission2.databinding.ActivityMainBinding
 import com.google.gson.Gson
 
@@ -12,6 +13,8 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
     lateinit var binding: ActivityMainBinding
     private var song:Song = Song()
     private var gson:Gson= Gson()
+
+    val songs=arrayListOf<Song>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +25,6 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
         initBottomNavigation()
 
         binding.mainPlayerCl.setOnClickListener {
-//            val intent = Intent(this,SongActivity::class.java)
             val editor=getSharedPreferences("song",MODE_PRIVATE).edit()
             editor.putInt("songId",song.id)
             editor.apply()
@@ -30,14 +32,13 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
             val intent=Intent(this, SongActivity::class.java)
             startActivity(intent)
 
-//            intent.putExtra("title", song.title)
-//            intent.putExtra("singer",song.singer)
-//            intent.putExtra("second",song.second)
-//            intent.putExtra("playTime",song.playTime)
-//            intent.putExtra("isPlaying",song.isPlaying)
-//            intent.putExtra("music",song.music)
-            startActivity(intent)
+        }
+        binding.mainNextIv.setOnClickListener {
+            moveSong(+1)
+        }
 
+        binding.mainPreviousIv.setOnClickListener {
+            moveSong(-1)
         }
 
     }
@@ -47,9 +48,34 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
         binding.mainMiniplayerTitleTv.text = title
         binding.mainMiniplayerSingerTv.text = singer
     }
+    private fun moveSong(direct: Int) {
+        val spf = getSharedPreferences("song", MODE_PRIVATE)
+        val songId = spf.getInt("songId", 0)
+
+        val nowPos = getPlayingSongPosition(songId)
+
+        val nextPos = nowPos + direct
+
+        val newSong = songs[nextPos]
+
+        val editor = spf.edit()
+        editor.putInt("songId", newSong.id)
+        editor.apply()
+
+        setMiniPlayer(newSong)
+
+        this.song = newSong
+    }
 
 
-
+    private fun getPlayingSongPosition(songId:Int): Int{
+        for(i in 0 until songs.size){
+            if(songs[i].id==songId){
+                return i
+            }
+        }
+        return 0
+    }
     private fun initBottomNavigation(){
 
         supportFragmentManager.beginTransaction()
@@ -96,15 +122,7 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
 
     override fun onStart() {
         super.onStart()
-        /*val sharedPreferences=getSharedPreferences("song",MODE_PRIVATE)
-        val songJson=sharedPreferences.getString("songData",null)
 
-        song=if(songJson==null) {
-            Song("라일락", "아이유(IU)", 0, 60, false, "unvailable")
-        }else{
-            gson.fromJson(songJson,Song::class.java)
-
-        }*/
         val spf=getSharedPreferences("song",MODE_PRIVATE)
         val songId=spf.getInt("songId",0)
 
@@ -123,10 +141,13 @@ class MainActivity : AppCompatActivity(),HomeFragment.OnSongPlayListener {
 
     private fun inputDummySongs(){
         val songDB = SongDatabase.getInstance(this)!!
-        val songs = songDB.songDao().getSongs()
+        val allSongs = songDB.songDao().getSongs()
 
-        if (songs.isNotEmpty()) return
-
+        if (allSongs.isNotEmpty()) {
+            songs.clear()
+            songs.addAll(allSongs)
+            return
+        }
         songDB.songDao().insert(
             Song(
                 "Lilac",
